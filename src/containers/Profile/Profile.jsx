5eb8c72bdadec5784f7fd89ab6d7c0595bd992e6
@carlosval <75422798+Carlos-Val@users.noreply.github.com>
2021-05-07 16:43:24 +0200

    input placeholder in view profile ok

--- src/containers/Profile/Profile.jsx
+++ src/containers/Profile/Profile.jsx
@@ -47,13 +47,13 @@ const Profile = (props) => {
     //HOOKS
 
     const [data, setUser] = useState({
-        name : "",
-        surname1 : "",
-        surname2 : "",
-        dni : "",
-        address : "",
-        city : "",
-        postalCode : ""
+        name : props.user[0].name,
+        surname1 : props.user[0].surname1,
+        surname2 : props.user[0].surname2,
+        dni : props.user[0].dni,
+        address : props.user[0].address,
+        city : props.user[0].city,
+        postalCode : props.user[0].postalCode
     });
 
 
@@ -118,39 +118,39 @@ const Profile = (props) => {
                     <div className="modifyForm">
                         <FormGroup>
                             <Label for='name'>Nombre:</Label>
-                            <Input type='text' value={props.user[0].name} id='name' name='name' onChange={handleState} valid={validationResult.validated && !validationResult.name} invalid={validationResult.validated && validationResult.name} />
+                            <Input type='text' placeholder={props.user[0].name} id='name' name='name' onChange={handleState} valid={validationResult.validated && !validationResult.name} invalid={validationResult.validated && validationResult.name} />
                             <FormFeedback>{validationResult.name}</FormFeedback>
                         </FormGroup>
                         <FormGroup>
                             <Label for='name'>Primer apellido:</Label>
-                            <Input type='text' value={props.user[0].surname1} id='surname1' name='surname1' onChange={handleState} valid={validationResult.validated && !validationResult.surname1} invalid={validationResult.validated && validationResult.surname1} />
+                            <Input type='text' placeholder={props.user[0].surname1} id='surname1' name='surname1' onChange={handleState} valid={validationResult.validated && !validationResult.surname1} invalid={validationResult.validated && validationResult.surname1} />
                             <FormFeedback>{validationResult.surname1}</FormFeedback>
                         </FormGroup>
                         <FormGroup>
                             <Label for='name'>Segundo apellido:</Label>
-                            <Input type='text' value={props.user[0].surname2} id='surname2' name='surname2' onChange={handleState} valid={validationResult.validated && !validationResult.surname2} invalid={validationResult.validated && validationResult.surname2} />
+                            <Input type='text' placeholder={props.user[0].surname2} id='surname2' name='surname2' onChange={handleState} valid={validationResult.validated && !validationResult.surname2} invalid={validationResult.validated && validationResult.surname2} />
                             <FormFeedback>{validationResult.surname2}</FormFeedback>
                         </FormGroup>
                         <FormGroup>
                             <Label for='name'>DNI:</Label>
-                            <Input type='text' value={props.user[0].dni} id='dni' name='dni' onChange={handleState} valid={validationResult.validated && !validationResult.dni} invalid={validationResult.validated && validationResult.dni} />
+                            <Input type='text' placeholder={props.user[0].dni} id='dni' name='dni' onChange={handleState} valid={validationResult.validated && !validationResult.dni} invalid={validationResult.validated && validationResult.dni} />
                             <FormFeedback>{validationResult.dni}</FormFeedback>
                         </FormGroup>
                     </div>
                     <div className="modifyForm">
                         <FormGroup>
                             <Label for='name'>Dirección:</Label>
-                            <Input type='text' value={props.user[0].address} id='address' name='address' onChange={handleState} valid={validationResult.validated && !validationResult.address} invalid={validationResult.validated && validationResult.address} />
+                            <Input type='text' placeholder={props.user[0].address} id='address' name='address' onChange={handleState} valid={validationResult.validated && !validationResult.address} invalid={validationResult.validated && validationResult.address} />
                             <FormFeedback>{validationResult.address}</FormFeedback>
                         </FormGroup>
                         <FormGroup>
                             <Label for='name'>Ciudad:</Label>
-                            <Input type='text' value={props.user[0].city} id='city' name='city' onChange={handleState} valid={validationResult.validated && !validationResult.city} invalid={validationResult.validated && validationResult.city} />
+                            <Input type='text' placeholder={props.user[0].city} id='city' name='city' onChange={handleState} valid={validationResult.validated && !validationResult.city} invalid={validationResult.validated && validationResult.city} />
                             <FormFeedback>{validationResult.city}</FormFeedback>
                         </FormGroup>
                         <FormGroup>
                             <Label for='name'>Codigo Postal:</Label>  
-                            <Input type='text' value={props.user[0].postalCode} id='postalCode' name='postalCode' onChange={handleState} valid={validationResult.validated && !validationResult.postalCode} invalid={validationResult.validated && validationResult.postalCode} />
+                            <Input type='text' placeholder={props.user[0].postalCode} id='postalCode' name='postalCode' onChange={handleState} valid={validationResult.validated && !validationResult.postalCode} invalid={validationResult.validated && validationResult.postalCode} />
                             <FormFeedback>{validationResult.postalCode}</FormFeedback>
                         </FormGroup>
                         <Button color='danger' onClick={() => updateUser()}>UPDATE</Button>
